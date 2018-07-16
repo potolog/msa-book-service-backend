822580030d8045d8587c947b432d5de4411ad138
amano <moneylog@naver.com>
2018-07-16 11:16:13 +0900

    reset

--- src/main/java/com/amano/springboot/repository/UserGroupRepository.java
+++
@@ -1,7 +0,0 @@
-package com.amano.springboot.repository;
-
-import com.amano.springboot.model.UserGroup;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface UserGroupRepository extends PagingAndSortingRepository<UserGroup, Long> {
-}
